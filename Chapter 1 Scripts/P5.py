4a3749ef0873b767c67ed6e8f3ca4c69310a6cf2
chiayolin <chiayo.lin@gmail.com>
2016-08-24 10:57:04 +0700

    completed P5

---
+++ Chapter 1 Scripts/P5.py
@@ -0,0 +1,19 @@
+# P5.py 
+# 
+# Write a simple Python program that prompts the user for a certain 
+# number of cities for the Traveling Salesman problem, and displays 
+# the total number of possible routes that can be taken. Your prog-
+# ram should function as shown below:
+# 
+#   How many cities? 10
+#   For 10 cities, there are 3628800 possible routes
+#
+# date:    08/25/2016
+# author:  Chiayo Lin
+# license: MIT License
+#
+
+import math
+
+(lambda n: print("for {} cities, there are {} possible routes".
+    format(n, math.factorial(n))))(int(input("how many cities? ")))
