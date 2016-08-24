a26df9776f1263fe82e37858500ba7f2e8c965d0
chiayolin <chiayo.lin@gmail.com>
2016-08-24 10:55:41 +0700

    completed P2

---
+++ Chapter 1 Scripts/P2.py
@@ -0,0 +1,17 @@
+# P2.py 
+#
+# write a Python program that allows the user to enter any integer va-
+# lue, and displays the value of 2 raised to that power. Your program 
+# should function as shown below:
+#
+#   what power of 2? 10
+#   2 to the power of 10 is 1024
+#
+# date:    08/25/2016
+# author:  Chiayo Lin
+# license: MIT License
+#
+
+# λeah
+(lambda x: print("2 to the power of", x, "is", 2**x))(\
+    int(input("what power of 2? ")))
