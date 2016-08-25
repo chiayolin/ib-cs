75ba0f3d65e1873c106986cb9941706c88a829db
chiayolin <chiayo.lin@gmail.com>
2016-08-25 12:15:32 +0700

    completed M1.py

---
+++ Chapter 1 Scripts/M1.py
@@ -0,0 +1,20 @@
+# M1.py
+#
+# Modify the sample “hello” Python program in section 1.6.5 to 
+# first request the user’s first name, and then request their 
+# last name. The program should then display:
+#
+#   Hello firstname lastname
+#   Welcome to Python!
+#
+# for the firstname and lastname entered.
+#
+# date:    08/24/2016
+# authors: Charles Dierbach, Chiayo Lin
+# license: n/a
+#
+
+firstname = input('What is your firstname? ')
+lastname  = input('What is your  lastname? ')
+print("Hello", firstname, lastname)
+print("Welcome to Python!")
