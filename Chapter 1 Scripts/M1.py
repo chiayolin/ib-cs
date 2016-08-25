f8ce1142c2a85116cbef6c0301a702c497b88b6c
chiayolin <chiayo.lin@gmail.com>
2016-08-25 17:26:04 +0700

    minor fix

--- Chapter 1 Scripts/M1.py
+++ Chapter 1 Scripts/M1.py
@@ -15,6 +15,6 @@
 #
 
 firstname = input('What is your firstname? ')
-lastname  = input('What is your  lastname? ')
+lastname  = input('What is your lastname? ')
 print("Hello", firstname, lastname)
 print("Welcome to Python!")
--- Chapter 1 Scripts/P4.py
+++ Chapter 1 Scripts/P4.py
@@ -1,25 +1,27 @@
 # P4.py 
 #
 # write a Python program that allows the user to enter a four
-# -digit _input number and displays its value in base 10. each 
-# _input digit should be entered one per line, starting with 
-# the leftmost digit , as shown below:
+# -digit input number and displays its value in base 10. each 
+# input digit should be entered one per line, starting with 
+# the leftmost digit, as shown below:
 #
-# Enter leftmost digit: 1
-# Enter the next digit: 0
-# Enter the next digit: 0
-# Enter the next digit: 1
-# The value is 9
+#   Enter leftmost digit: 1
+#   Enter the next digit: 0
+#   Enter the next digit: 0
+#   Enter the next digit: 1
+#   The value is 9
 #
 # date:    08/25/2016
 # author:  Chiayo Lin
 # license: MIT License
 #
 
-_range = 4
-_input = [0] * _range
+# init 
+_range = 4 # since it's asking for a 4-digit number
+_input = [0] * _range # declare an array with size `_range`
 _tuple = zip(range(0, _range), list(reversed(range(_range))))
 
+# main
 prompt = ("leftmost", "the next")
 for place, power in _tuple:
     print("Enter", prompt[1 if place else 0], "digit: ", end = '')
