a567e36d06c4579462e08da80d89b8b7727796e9
chiayolin <chiayo.lin@gmail.com>
2016-09-05 22:29:46 +0700

    update comment for P5

--- Chapter 2 Scripts/P5.py
+++ Chapter 2 Scripts/P5.py
@@ -1,7 +1,7 @@
 # P5.py
 #
-# this program prompts user for two integer input and display the following 
-# arithmetic operations, e.g., if the user enters the values 7 and 5:
+# this program prompts user for two integer input and display the following ari-
+# thmetic operations, e.g., if the user enters the values 7 and 5:
 # 
 #   7 + 5 = 12
 #   7 - 5 = 2
@@ -10,6 +10,9 @@
 #   7 // 5 = 1 
 #   7 % 5 = 2
 #   7 ** 5 = 16,807
+#
+# All floating point results should be displayed with two decimal places of acc-
+# uracy. In addtion, all values should be displayed with commas where approiate.
 #
 # date:    08/31/2016
 # author:  Chiayo Lin
@@ -26,3 +29,14 @@ operations = lambda x, y: \
 for operator, result in operations(numero_uno, numero_dos):
     print(numero_uno, operator, numero_dos, '=', 
         type(result) is str and result or format(result, ','))
+
+'''
+10/10 Execution
+10/10 Solution
+8/8 Output
+10/10 Logic
+7/7 Style
+5/5 Documentation
+
+50/50 Total
+'''
