941d5c3284afaf6ef1eacfa9988b90a6a10a58d9
chiayolin <chiayo.lin@gmail.com>
2016-09-28 21:55:44 +0700

    update P4

--- Chapter 3 Scripts/P4.py
+++ Chapter 3 Scripts/P4.py
@@ -1,22 +1,16 @@
 # P4.py
 #
 # Write a program that sums a series of (positive) integers entered by the 
-# user, excluding all numbers that are greater than 100.
+# user, excluding all terms that are greater than 100.
 #
 # date:    09/25/2016
 # author:  Chiayo Lin
 # license: GPL 3.0
 
-# "functional" style so no loops are used.
+print("Numbers greater than 100 will be excluded.")
 
-# print instruction for the user
-print("Enter a list of numbers, separate by commas,",
-      "numbers greater than 100 will be excluded:", sep = '\n')
-
-# get a list of integers and remove non-digits, commas, and whitespaces
-str_to_int = lambda term: term.strip().isdigit() and int(term)
-sequence = list(map(str_to_int, input(">>> ").split(',')))
-
-# filter out numbers greater than 100, sum the rest, and print the result
-series = sum(list(filter(lambda term: term < 100, sequence)))
-print("The sum of the sequence you entered is ", series, '.', sep = '')
+series = term = 0
+while term >= 0:
+    term = int(input("Enter an integer (-1 to end): "))
+    series += term <= 100 and term >= 0 and term
+print("The sum of the sequence is ", series, '.', sep = '')
