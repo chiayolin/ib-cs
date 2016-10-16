f79396e9c369b153770b25915f4740fc340ec358
chiayolin <chiayo.lin@gmail.com>
2016-10-16 21:08:49 +0800

    P7 in scheme

---
+++ Chapter 3 Scripts/P7.scm
@@ -0,0 +1,29 @@
+; P7.scm
+;
+; Display the integer values 1–100 as given in question P6 using Scheme.
+;
+; date:    10/16/2016
+; author:  Chiayo Lin
+; license: GPL 3.0
+
+(define divisible? 
+  (lambda (x y) 
+    (= (remainder x y) 0)))
+
+(define print-format
+  (lambda (number right-algin)
+    (if (= (string-length (number->string number)) right-algin)
+      (display number)
+      (begin
+        (display " ")
+        (print-format number (- right-algin 1))))))
+
+(define print-row 
+  (lambda (counter)    
+    (print-format counter 4) 
+    (if (divisible? counter 10) 
+      (newline))
+    (if (not (= counter 100)) 
+      (print-row (+ counter 1)))))
+
+(print-row 1)
