53af53e749a32333e4eeef4f9b0e39a53b614861
chiayolin <chiayo.lin@gmail.com>
2016-10-31 19:47:37 +0700

    updated T1.py

--- Chapter 3 Scripts/T1.py
+++ Chapter 3 Scripts/T1.py
@@ -6,12 +6,12 @@
 
 end = 0
 while not end:
-    integer_1 = int(input("\nEnter first integer: "))
-    integer_2 = int(input("Enter second integer: "))
-    integer_3 = int(input("Enter last integer: "))
+    num_1 = int(input("\nEnter first integer: "))
+    num_2 = int(input("Enter second integer: "))
+    num_3 = int(input("Enter last integer: "))
 
-    print((integer_1 in (integer_2, integer_3) or              \
-           integer_2 in (integer_1, integer_3)) and            \
+    print((num_1 in (num_2, num_3) or                          \
+           num_2 in (num_1, num_3)) and                        \
            "Duplicate values found" or "Numbers unique")
 
     end = 0 if input("\nPlay again (y/n)? ").lower()[0] is 'y' \
