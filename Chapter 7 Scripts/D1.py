b2c5b33fe8cbe8edb9badab0537c2d0d8157f2d1
chiayolin <chiayo.lin@gmail.com>
2017-02-07 14:04:09 +0700

    updated D1.py

--- Chapter 7 Scripts/D1.py
+++ Chapter 7 Scripts/D1.py
@@ -10,57 +10,41 @@
 
 def parenMatch(string):
     
-    """
-    NAME
-        parenMatch -- checks if all parentheses match
+    """ Returns True if all parentheses match else returns False.
     
-    SYNOPSIS
-        parenMatch(<class 'str'>)
-
-    DESCRIPTION
         The parenMatch() function determines if all parentheses in a string
         match. This includes the following charecter pairs:
 
             "{"    "}"    "["    "]"    "("    ")"
         
         Pairs of parentheses may be nested.
-    
-    RETURN VALUES
-        parenMatch() returns True if all parentheses match else returns False.
-    
-    HISTORY
-        parenMatch() first appeared in D1.py in 2017.
-
-    BUGS
-        A lot.
     """
 
-    def _tokenizer(string):
-        return [*map(lambda x: x in "{[(" and '(' or x in ")]}" and ')',
-                     filter(lambda x: x in "{[()]}", string))]
+    def _tokenize(string):
+        return [*filter(lambda x: x in "{[()]}", string)]
         
     def _parenMatch(tokens, index = 0):
         if not len(tokens):            return True
         elif index == len(tokens) - 1: return False
         
-        return tokens[index] == '(' and tokens[index + 1] == ')' and \
-               _parenMatch(tokens[:index] + tokens[index + 2:])  or  \
+        return (tokens[index] == '(' and tokens[index + 1] == ')') or  \
+               (tokens[index] == '[' and tokens[index + 1] == ']') or  \
+               (tokens[index] == '{' and tokens[index + 1] == '}') and \
+               _parenMatch(tokens[:index] + tokens[index + 2:]) or  \
                _parenMatch(tokens, index + 1)
         
-    return _parenMatch(_tokenizer(string))
+    return _parenMatch(_tokenize(string))
 
 # Tests
 if __name__ == "__main__":
-    test_cases = ["", "()", "(())", "{[()]}", "{}{}()()[]", "{(([[[]]]()))}",
+    test_cases = ["", "(]", "((])", "{[()]}", "{}{}()()[]", "{}{",
                   "(define add2 (lambda x (+ x 2)))", "int main() { return 0 }",
-                  ")(", ")))[}(((", "{}[[[{{})", "range(1, 2", "{}()()){{))]]]()"]
-
+                  ")(", ")))[}(((", "{}[[[{{})", "range(1, 2", ")))(("]
+    
     for case in test_cases:
-        print(">>>", case)
-        if parenMatch(case):
-            print("--> All parentheses matched.")
-        else:
-            print("--> Unmatched parentheses.")
-        
-        print()
+        print(format(case, '<30'), end='--> ')
 
+        if parenMatch(case):
+            print("All parentheses matched")
+        else:
+            print("Parentheses unmatched") 
