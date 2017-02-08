d411d5fcfeb5d74ee7a8bacbb791a4a064c311c8
chiayolin <chiayo.lin@gmail.com>
2017-02-09 05:58:12 +0700

    completed P1

---
+++ Chapter 8 Scripts/P1.py
@@ -0,0 +1,21 @@
+# P1.py
+#
+# Write a Python function called reduceWhitespace that is given a line read 
+# from a text  file and returns the line with all extra whitespace characters 
+# between words removed: 
+#   ‘This line has extra space characters’ --> 
+#   ‘This line has extra space characters’
+#
+# date:    02/09/2017
+# author:  Chiayo Lin
+# license: GPL 3.0
+
+from functools import reduce
+
+def reduceWhitespace(line):
+    
+    """ 
+    Returns line with extra whitespaces removed.
+    """
+    
+    return reduce(lambda h, t: h + ' ' + t, line.split()) 
