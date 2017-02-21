d431921a4bd20aebce9137fcdce57b61371e16a4
chiayolin <chiayo.lin@gmail.com>
2017-02-21 14:21:03 +0700

    fix M6

--- Chapter 8 Scripts/M6.py
+++ Chapter 8 Scripts/M6.py
@@ -15,8 +15,7 @@ import re
 def getFile():
     
     """
-    Returns the file name and associated file object for reading the
-    file  as tuple of the form (file_name, input_file).
+    Returns file name and file object as tuple: (file_name, input_file).
     """
     
     input_file_opened = False
@@ -30,57 +29,38 @@ def getFile():
             
     return (file_name, input_file)
 
-def countWords(input_file, search_word):
+def countWords(input_file):
     
     """ 
-    Returns the number of occurrences of search_word in the provided 
-    input_file file object.
+    Returns number of occurrences of search_word in provided file
     """
 
-    def tokenize(lines):
-        tokens = []
-        delimiters = "[, \-!?:()\n\'\"]+"
-        for line in lines:
-            tokens += [*filter(None, re.split(delimiters, line))]
-        
-        return tokens
-   
-    return len([*filter(lambda t: t == search_word, tokenize(input_file))])
+    tokens = []
+    delimiters = "[, \-!?:()\n\'\"]+"
+    for line in input_file:
+        tokens += [*filter(None, re.split(delimiters, line))]
+    
+    word_count = {} 
+    for token in tokens:
+        if token in word_count:
+            word_count[token] += 1
+        else:
+            word_count[token] = 1
 
-def writeCountWordList(input_file, word_list):
+    return word_count
+
+def writeResult(word_count, file_name):
     
     """
-    Writes all the word counts base on word_list file object in input_file 
-    file object file to a text file called input_file.name + ".wc".
+    Writes result of word count to file ending with '.wc'.
     """
-    
-    def makeResult(file_name, search_word, num_occurrences):
-        return not num_occurrences and "No occurrences of word '" +     \
-               search_word + "' found in file '" + file_name + "'\n" or \
-               "The word '" + search_word + "' occurs " +               \
-               str(num_occurrences) + " times in file '" + file_name + "'\n"
-    
-    wc_file = open(input_file.name + ".wc", 'w')
 
-    for search_word in word_list:
-        input_file.seek(0) # reset file pointer
-        wc_file.write(makeResult(input_file.name, search_word,
-                                 countWords(input_file, search_word)))
+    output_file = open(file_name + ".wc", 'w')
+    for word in word_count:
+        output_file.write("'" + word + "', " + str(word_count[word]) + "\n")
 
     return
 
-def getWordList(word_list_file = "M6_text.txt"):
-
-    """
-    Returns a list of words without newlines from word_list_file.
-    """
-    
-    word_list = []
-    for word in open(word_list_file, 'r'):
-        word_list += [word[:-1]]
-
-    return word_list
-    
 ## main
 
 # program welcome
@@ -88,7 +68,7 @@ print('This program will display the number of occurrences of a')
 print('specified word within a given text file\n')
 
 file_name, input_file = getFile()
-writeCountWordList(input_file, getWordList())
+writeResult(countWords(input_file), file_name)
 
 print("Result is written to '" + file_name + ".wc'")
 
