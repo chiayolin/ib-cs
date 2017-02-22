37f6be3676a562851c7d72ce47523e50cf5e9e84
chiayolin <chiayo.lin@gmail.com>
2017-02-22 13:03:37 +0700

    completed D2

--- Chapter 8 Scripts/D2.py
+++ Chapter 8 Scripts/D2.py
@@ -6,10 +6,24 @@
 # word “I”). Consider how readable the results are for various sample text.
 #
 # date:    02/21/2017
+# author:  Chiayo Lin
 # license: GPL 3.0
 
-import sys, re
+import sys, os, re
 
+def getFile(prompt, flag):
+    try:
+        return open(input(prompt + ": "), flag)
+    
+    except IOError:
+        sys.stderr.write("Error: file cannot be read.\n")
+        sys.exit(os.EX_IOERR) # use Unix exit code
+
+def readFile(i_file):
+    for line in i_file:
+        for word in re.split('(\s+)', line):
+            yield word
+ 
 def removeVowel(word):
     index = found = 0
     while not (index == len(word) or found):
@@ -19,21 +33,9 @@ def removeVowel(word):
     # index - 1 because it's off by 1
     result = word[:index - 1] + word[index:]
     
-    # returns word if removal left empty word (e.g. I)
+    # return word if removal left empty word (e.g. I)
     return not (result and found) and word or result
-
-def getFile(prompt, flag):
-    try:
-        return open(input(prompt + ": "), flag)
-    
-    except IOError:
-        sys.stderr.write("Error: file cannot be read.")
-
-def readFile(i_file):
-    for line in i_file:
-        for word in re.split('(\s+)', line):
-            yield word
-            
+           
 if __name__ == "__main__":
     i_file = getFile("Locate a text file", 'r')
     o_file = open(i_file.name + ".rv", 'w')
